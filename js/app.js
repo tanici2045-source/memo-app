61b658eeba6b83e0420c84d16c10dc33e077b270
tanici2045-source <tanici2045@gmail.com>
2026-01-09 11:17:21 +0900

    Add utility function to get current date and time

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // js/app.js
 window.App = window.App || {};
 App.state = App.state || { memos: [] };
+App.util = App.util || {};
+App.util.nowStr = App.util.nowStr || (() => {
+  const pad = (n) => String(n).padStart(2,"0");
+  return () => {
+    const d = new Date();
+    return `${d.getFullYear()}-${pad(d.getMonth()+1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}`;
+  };
+})();
